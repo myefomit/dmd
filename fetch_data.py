ae64c46f9ac7cd3b3c310e5b9354074e481ce2ff
frmer <fvrmer@yahoo.com>
2015-09-13 22:47:38 +0300

    add new feature

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,4 @@
-import urllib #ololo
+import urllib #ololo hello from new feature
 url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
 data = urllib.request.urlopen(url).read()
 print(data)
