7cfc2cc4bf7827a2c1f8b28b36456323d6342af1
tomcat1911 <maslovtimofey@gmail.com>
2015-09-13 22:50:25 +0300

    add new tuples

--- dbtest.py
+++ dbtest.py
@@ -7,6 +7,8 @@ cur.execute("CREATE TABLE test (first_name varchar(255), last_name varchar(255))
 cur.execute("INSERT INTO test (first_name, last_name) VALUES ('Bugster', 'Petrov');")
 cur.execute("INSERT INTO test (first_name, last_name) VALUES ('Kot', 'Ivanov');")
 cur.execute("INSERT INTO test (first_name, last_name) VALUES ('Tuzik', 'unknown');")
+cur.execute("INSERT INTO test (first_name, last_name) VALUES ('Bugster', 'Fomenko');")
+cur.execute("INSERT INTO test (first_name, last_name) VALUES ('Bugster', 'Gribnoy');")
 
 cur.execute("SELECT * FROM test;")
 data = cur.fetchall()
--- view/search_result.tpl
+++ view/search_result.tpl
@@ -1,6 +1,10 @@
-%for row in rows:
-  <ul>
-    <li>First Name: {{row[0]}}</li>
-    <li>Last Name: {{row[1]}}</li>
-  </ul>
+%if (rows != []):
+  %for row in rows:
+    <ul>
+      <li>First Name: {{row[0]}}</li>
+      <li>Last Name: {{row[1]}}</li>
+    </ul>
+  %end
+%else:
+  <h1>Nothing for you today, come back soon!</h1>
 %end
