1b139f27eb7d9bb4da852ae69babc3b2f3179eef
tomcat1911 <maslovtimofey@gmail.com>
2015-09-15 23:39:04 +0300

    unecessary comment

--- fetch_data.py
+++ fetch_data.py
@@ -1,5 +1,5 @@
 import urllib 
-url = 'http://export.arxiv.org/api/query?search_query=all&start=0&max_results=10000'
+url = 'http://export.arxiv.org/api/query?search_query=all&start=0&max_results=10000' #10 000 is max
 data = urllib.urlopen(url).read()
 f = open('result.txt', 'w')
 f.write(data)
