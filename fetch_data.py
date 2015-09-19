3d245335e71ffe6cd8fc6beb7191244d3e06fdb7
frmer <fvrmer@yahoo.com>
2015-09-19 17:34:57 +0300

    new fetch data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,11 +1,11 @@
 import urllib
 import feedparser
 
-# базовый url 
+# base url
 base_url = 'http://export.arxiv.org/api/query?';
 
-# параметры поиска
-search_query = 'all:electron' # ищем по слову
+# serach parametrs
+search_query = 'all:пизда' # search for word
 start = 0
 max_results = 10
 
@@ -13,34 +13,31 @@ query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
                                                      start,
                                                      max_results)
 
-# начнем это
+# let's do this shit
 feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
 feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
 
-# выполняем запрос get с помощью базового url и запроса
+# GET request using the base_url and query
 response = urllib.urlopen(base_url+query).read()
 
-#парсим ответ с помощью feedparser
+# parse the response using feedparser
 feed = feedparser.parse(response)
 
-# печатаем инфу
-print ('Feed title: %s' % feed.feed.title)
-print ('Feed last updated: %s' % feed.feed.updated)
+# print some information
+print('Feed title: %s' % feed.feed.title)
+print('Feed last updated: %s' % feed.feed.updated)
 
-# печатаем opensearch metadata
-print ('totalResults for this query: %s' % feed.feed.opensearch_totalresults)
-print ('itemsPerPage for this query: %s' % feed.feed.opensearch_itemsperpage)
-print ('startIndex for this query: %s'   % feed.feed.opensearch_startindex)
+# print opensearch metadata
+print('totalResults for this query: %s' % feed.feed.opensearch_totalresults)
+print('itemsPerPage for this query: %s' % feed.feed.opensearch_itemsperpage)
+print('startIndex for this query: %s'   % feed.feed.opensearch_startindex)
 
-# печатаем инфу для каждого вхождения
+# print information for each entry
 for entry in feed.entries:
-    print ('e-print metadata')
-    print ('arxiv-id: %s' % entry.id.split('/abs/')[-1])
-    print ('Published: %s' % entry.published)
-    print ('Title:  %s' % entry.title)
-    
-    # feedparser v4.1 получает только первого автора, а не всех
-    author_string = entry.author
+    print('e-print metadata')
+    print('arxiv-id: %s' % entry.id.split('/abs/')[-1])
+    print('Published: %s' % entry.published)
+    print('Title:  %s' % entry.title)
     
     # подчиняем себе <arxiv:affiliation>
     try:
@@ -48,42 +45,43 @@ for entry in feed.entries:
     except AttributeError:
         pass
     
-    print ('Last Author:  %s' % author_string)
+    print('Last Author:  %s' % author_string)
     
     # обработать несколько авторов и напечатать их (feedparser 5.0.1)
     try:
-        print ('Authors:  %s' % ', '.join(author.name for author in entry.authors))
+        print('Authors:  %s' % ', '.join(author.name for author in entry.authors))
     except AttributeError:
         pass
 
-    # получить ссылки на pdf или abs
+    # get links to .pdf or abs
     for link in entry.links:
         if link.rel == 'alternate':
-            print ('abs page link: %s' % link.href)
+            print('abs page link: %s' % link.href)
         elif link.title == 'pdf':
-            print ('pdf link: %s' % link.href)
+            print('pdf link: %s' % link.href)
     
-    # под пространством имен АрХив имеются такие разделы, как:
-    # journal reference, comments и primary_category
+    # The journal reference, comments and primary_category 
+    # sections live under the arxiv namespace
     try:
         journal_ref = entry.arxiv_journal_ref
     except AttributeError:
         journal_ref = 'No journal ref found'
-    print ('Journal reference: %s' % journal_ref)
+    print('Journal reference: %s' % journal_ref)
     
     try:
         comment = entry.arxiv_comment
     except AttributeError:
         comment = 'No comment found'
-    print ('Comments: %s' % comment)
+    print('Comments: %s' % comment)
     
-    #  с того момента, как <arxiv:primary_category> не имеет данных -
-    # прекратить делать эту хуйню
-    print ('Primary Category: %s' % entry.tags[0]['term'])
+    # Since the <arxiv:primary_category> element has no data,
+    # feedparser does not store anything inside
+    # entry.arxiv_primary_category
+    print('Primary Category: %s' % entry.tags[0]['term'])
     
-    # получаем все категории 
+    # get all the categories
     all_categories = [t['term'] for t in entry.tags]
-    print ('All Categories: %s' % (', ').join(all_categories))
+    print('All Categories: %s' % (', ').join(all_categories))
     
-    # <suumary>
-    print ('Abstract: %s' %  entry.summary)
+    # The abstract is in the <summary> element
+    print('Abstract: %s' %  entry.summary)
