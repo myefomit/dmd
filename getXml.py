3367f8eb8805ebece280fe362a184a349cb8f3a3
frmer <fvrmer@yahoo.com>
2015-09-21 20:45:36 +0300

    hurray, getxml.py work

--- getXml.py
+++ getXml.py
@@ -1,7 +1,9 @@
 import urllib.request
 
-f = urllib.request.urlopen('http://export.arxiv.org/api/query?search_query=all&start=0&max_results=1')
-myfile = f.read()
-f = open('file.xml', 'w')
-f.write(str(myfile))
+f = open('file.txt', 'w')
+for i in range (0,1000):
+    data = urllib.request.urlopen("http://export.arxiv.org/api/query?search_query=all&start=" + str(i * 1000)  + "&max_results=1000").read()
+    print(i)
+    f.write(str(data))
+    f.write("\n\n")
 f.close()
