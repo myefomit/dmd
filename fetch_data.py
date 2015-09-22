71a07861720bd7c8df0773f355f191a476307c73
frmer <fvrmer@yahoo.com>
2015-09-22 21:27:00 +0300

    working parser, finally

--- fetch_data.py
+++ fetch_data.py
@@ -1,9 +1,13 @@
-from xml.dom.minidom import parseString
-file = open('file.xml', 'w')
-data = file.read()
-file.close()
-dom = parseString(data)
-tilteTag = dom.getElementsByTagTitle('title')[0].toxml()
-tilte=tilteTag.replace('<title>','').replace('</title>','')
-f. = open('base.xml', 'w')
-f.write(data)
+from xml.dom.minidom import parse, parseString
+
+document = parse('file.txt')
+for i in range (0, 100):
+    try:
+        publishedTag = document.getElementsByTagName('published')[0].toxml()
+        published = publishedTag.replace('<published>','').replace('</published>','')
+        print(published)
+        summaryTag = document.getElementsByTagName('summary')[0].toxml()
+        summary = summaryTag.replace('<summary>','').replace('</summary>','')
+        print(summary)
+    except:
+        continue
--- getXml.py
+++ getXml.py
@@ -2,8 +2,9 @@ import urllib.request
 
 f = open('file.txt', 'w')
 for i in range (0,1000):
-    data = urllib.request.urlopen("http://export.arxiv.org/api/query?search_query=all&start=" + str(i * 1000)  + "&max_results=1000").read()
+    data = urllib.request.urlopen("http://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=arXiv").read()
     print(i)
     f.write(str(data))
-    f.write("\n\n")
 f.close()
+
+
