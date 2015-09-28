2176486c0d9eef5a0e5cf89e21825cb8b9405e3b
Jeaced <sergey_umnov@mail.ru>
2015-09-28 21:49:45 +0300

    Changed database schema

--- setup.sql
+++ setup.sql
@@ -25,6 +25,25 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET search_path = public, pg_catalog;
 
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: articles; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE articles (
+    id character varying(9) NOT NULL,
+    title character varying(255),
+    year integer,
+    link character varying(255),
+    summary character varying(255)
+);
+
+
+ALTER TABLE public.articles OWNER TO postgres;
+
 --
 -- Name: author_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
@@ -39,52 +58,23 @@ CREATE SEQUENCE author_id_seq
 
 ALTER TABLE public.author_id_seq OWNER TO postgres;
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
 --
 -- Name: authors; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE authors (
-    author_id integer DEFAULT nextval('author_id_seq'::regclass) NOT NULL,
-    name character varying(255),
-    surname character varying(255)
+    aid integer DEFAULT nextval('author_id_seq'::regclass) NOT NULL,
+    name character varying(255)
 );
 
 
 ALTER TABLE public.authors OWNER TO postgres;
 
 --
--- Name: connection; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+-- Name: category_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE connection (
-    pub_id integer,
-    author_id integer
-);
-
-
-ALTER TABLE public.connection OWNER TO postgres;
-
---
--- Name: keywords; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE keywords (
-    pub_id integer,
-    keywords character varying(255)
-);
-
-
-ALTER TABLE public.keywords OWNER TO postgres;
-
---
--- Name: pub_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE pub_id_seq
+CREATE SEQUENCE category_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -92,23 +82,40 @@ CREATE SEQUENCE pub_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.pub_id_seq OWNER TO postgres;
+ALTER TABLE public.category_id_seq OWNER TO postgres;
 
 --
--- Name: publications; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+-- Name: categories; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
-CREATE TABLE publications (
-    pub_id integer DEFAULT nextval('pub_id_seq'::regclass) NOT NULL,
-    title character varying(255),
-    theme character varying(255),
-    category character varying(255),
-    journal character varying(255),
-    link character varying(255)
+CREATE TABLE categories (
+    cid integer DEFAULT nextval('category_id_seq'::regclass) NOT NULL,
+    category character varying(255)
 );
 
 
-ALTER TABLE public.publications OWNER TO postgres;
+ALTER TABLE public.categories OWNER TO postgres;
+
+--
+-- Name: connection; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE connection (
+    id character varying(9),
+    aid integer,
+    cid integer
+);
+
+
+ALTER TABLE public.connection OWNER TO postgres;
+
+--
+-- Data for Name: articles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY articles (id, title, year, link, summary) FROM stdin;
+\.
+
 
 --
 -- Name: author_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
@@ -121,39 +128,39 @@ SELECT pg_catalog.setval('author_id_seq', 1, false);
 -- Data for Name: authors; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY authors (author_id, name, surname) FROM stdin;
+COPY authors (aid, name) FROM stdin;
 \.
+
+
+--
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY categories (cid, category) FROM stdin;
+\.
+
+
+--
+-- Name: category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('category_id_seq', 1, false);
 
 
 --
 -- Data for Name: connection; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY connection (pub_id, author_id) FROM stdin;
+COPY connection (id, aid, cid) FROM stdin;
 \.
 
 
 --
--- Data for Name: keywords; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Name: articles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
-COPY keywords (pub_id, keywords) FROM stdin;
-\.
-
-
---
--- Name: pub_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('pub_id_seq', 1, false);
-
-
---
--- Data for Name: publications; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY publications (pub_id, title, theme, category, journal, link) FROM stdin;
-\.
+ALTER TABLE ONLY articles
+    ADD CONSTRAINT articles_pkey PRIMARY KEY (id);
 
 
 --
@@ -161,39 +168,39 @@ COPY publications (pub_id, title, theme, category, journal, link) FROM stdin;
 --
 
 ALTER TABLE ONLY authors
-    ADD CONSTRAINT authors_pkey PRIMARY KEY (author_id);
+    ADD CONSTRAINT authors_pkey PRIMARY KEY (aid);
 
 
 --
--- Name: publications_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+-- Name: categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
-ALTER TABLE ONLY publications
-    ADD CONSTRAINT publications_pkey PRIMARY KEY (pub_id);
-
-
---
--- Name: connection_author_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY connection
-    ADD CONSTRAINT connection_author_id_fkey FOREIGN KEY (author_id) REFERENCES authors(author_id);
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (cid);
 
 
 --
--- Name: connection_pub_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: connection_aid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY connection
-    ADD CONSTRAINT connection_pub_id_fkey FOREIGN KEY (pub_id) REFERENCES publications(pub_id);
+    ADD CONSTRAINT connection_aid_fkey FOREIGN KEY (aid) REFERENCES authors(aid) ON UPDATE CASCADE ON DELETE SET NULL;
 
 
 --
--- Name: keywords_pub_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: connection_cid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY keywords
-    ADD CONSTRAINT keywords_pub_id_fkey FOREIGN KEY (pub_id) REFERENCES publications(pub_id);
+ALTER TABLE ONLY connection
+    ADD CONSTRAINT connection_cid_fkey FOREIGN KEY (cid) REFERENCES categories(cid) ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+--
+-- Name: connection_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY connection
+    ADD CONSTRAINT connection_id_fkey FOREIGN KEY (id) REFERENCES articles(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
